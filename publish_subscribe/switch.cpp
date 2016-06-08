2566b9cfd9178af51e5b2ceafadec14fe72409d6
xtofl <kristoffel.pirard@gmail.com>
2016-06-08 14:29:35 +0200

    [impl] make Switch draggable

--- publish_subscribe/switch.cpp
+++ publish_subscribe/switch.cpp
@@ -3,6 +3,7 @@
 #include <QSizeF>
 #include <QPainter>
 #include <QDebug>
+#include <QGraphicsSceneMouseEvent>
 
 Switch::Switch(QPointF topleft, QString name, QObject* objectParent, QGraphicsItem *parent)
     : QObject(objectParent)
@@ -19,5 +20,16 @@ void Switch::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QW
 void Switch::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     qDebug() << "pressed";
+    Q_ASSERT(event != nullptr);
+    moving.from = event->pos();
+    emit clicked();
+}
+
+void Switch::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    qDebug() << "release";
+    Q_ASSERT(event != nullptr);
+    moving.to = event->pos();
+    setPos(pos() + (moving.diff()));
     emit clicked();
 }
--- publish_subscribe/switch.h
+++ publish_subscribe/switch.h
@@ -10,12 +10,22 @@ class Switch : public QObject, public QGraphicsRectItem
 public:
     Switch(QPointF topleft, QString name, QObject *objectParent, QGraphicsItem *parent);
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
-    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+
 
 signals:
     void clicked();
 private:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+
     QString name;
+
+    struct Moving {
+        QPointF from;
+        QPointF to;
+        QPointF diff() const { return to - from;}
+    };
+    Moving moving;
 };
 
 #endif // SWITCH_H
